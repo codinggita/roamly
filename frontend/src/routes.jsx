7d57dab2b676bfdbfd9262fe82a44235e3d429bf
Rutvi <rutvi.parakhiya.cg@gmail.com>
2026-04-29 08:23:39 +0530

    routes implementation

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,16 +1,10 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/home/Home"
-import WiFiFinder from "./pages/wifi/WiFiFinder"
-import MenuDecoder from "./pages/menu/MenuDecoder"
-import DoctorFinder from "./pages/doctor/DoctorFinder"
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/wifi" element={<WiFiFinder />} />
-      <Route path="/menu" element={<MenuDecoder />} />
-      <Route path="/doctors" element={<DoctorFinder />} />
     </Routes>
   )
 }
